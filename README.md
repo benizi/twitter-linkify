be97c697cc7c0d48a55ede3a7a3e7d0fa53de8a4
Benjamin R. Haskell <twitter@benizi.com>
2013-11-13 01:42:00 -0500

    Adds unpacked script with simple README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Twitter Linkify
+
+Simple Chrome plugin to turn '@username' in text nodes into links to Twitter.
+
+Currently only exists as an unpacked extension (requires "developer mode").
---
+++ content_script.js
@@ -0,0 +1,44 @@
+var twitterUsername = /((?:^|\s)@[A-Za-z0-9_]{1,15})\b/;
+
+function processMatching(root, match, callback) {
+  var nodes = [];
+
+  var walker = document.createTreeWalker(root, NodeFilter.SHOW_TEXT);
+
+  while (walker.nextNode())
+    if (walker.currentNode.nodeValue.match(match))
+      nodes.push(walker.currentNode);
+
+  nodes.forEach(callback);
+}
+
+function linkify(node) {
+  var txt = node.nodeValue,
+      parts = txt.split(twitterUsername);
+
+  // nothing to do if we didn't match
+  if (parts.length == 1 && parts[0] == txt) return;
+
+  var par = node.parentNode;
+  parts.forEach(function(txt) {
+    var newNodes = [];
+    if (txt.match(twitterUsername)) {
+      var prefix = txt.match(/^\s*@/)[0],
+          id = txt.substr(prefix.length),
+          link = document.createElement('a');
+      link.appendChild(document.createTextNode(id));
+      link.setAttribute('href', "https://twitter.com/" + id);
+      newNodes.push(document.createTextNode(prefix));
+      newNodes.push(link);
+    } else {
+      newNodes.push(document.createTextNode(txt));
+    }
+    newNodes.forEach(function(newNode) {
+      par.insertBefore(newNode, node);
+    });
+  });
+
+  par.removeChild(node);
+}
+
+processMatching(document.body, twitterUsername, linkify);
